6b1e70b9d3fec80768812bbed61d9cff161230ef
jlorebu <jeibeltranu@gmail.com>
2023-04-12 10:52:36 -0500

    config-server

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from 'express'; 
+import { port } from "./config/index.js";
+
+const app = express(); 
+
+app.get('/', (request, response, error) => {
+    response.send('status: ok')
+})
+
+app.listen(port, (error) => {
+    if(error){
+        console.log('Server Error: Failed'); 
+        process.exit(1); 
+    }
+
+    console.log(`Server listening in port ${port}`)
+})
