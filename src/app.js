cb6ce5da38b219439ad04e8f5be9fe68f65bf941
jlorebu <jeibeltranu@gmail.com>
2023-04-14 19:51:41 -0500

    bd connection y morgan

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
+import morgan from 'morgan';
 import express from 'express'; 
+import { bdConnection } from './config/db.js';
 import { port } from "./config/index.js";
 
 const app = express(); 
+
+bdConnection()
 
 app.get('/', (request, response, error) => {
     response.send('status: ok')
---
+++ src/config/db.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import { dbUri } from './index.js';
+
+
+
+export const bdConnection = async () => {
+    try{
+        await mongoose.connect(dbUri);
+        console.log('Conexión exitosa a la base de datos');
+    }catch (err){
+        console.log(err, 'Conexion fallida a la base de datos');
+    }
+};
+
+
+
+bdConnection();
