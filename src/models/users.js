9f19f3a48585a1dfb704aa3fd8ea0009a914b84b
jlorebu <jeibeltranu@gmail.com>
2023-05-04 16:20:43 -0500

    userSchema

--- src/models/users.js
+++ src/models/users.js
@@ -1,3 +1,16 @@
-
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
 //schema y modelo de mongoose
+
+const userSchema = new Schema({
+    firstName: {String, require:true}, 
+    lastName: {String, require:true}, 
+    email: {String, require:true},
+    rol: {String, require:true},
+    creationDate: {Date, default: Date.now()},  
+})
+
+
+const User = mongoose.model('User', userSchema); 
+export default User; 
