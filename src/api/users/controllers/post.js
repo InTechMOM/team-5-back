26a8c97b3a8f5b8302d25a14cbbb70b615a2f3d7
jlorebu <jeibeltranu@gmail.com>
2023-05-05 00:10:40 -0500

    joi schema

--- src/api/users/controllers/post.js
+++ src/api/users/controllers/post.js
@@ -1,9 +1,19 @@
 // schema JOI
-
+import Joi from 'joi';
 import express from 'express'; 
-const router = express.Router();
 
-router.post('/', (req, res) => {
+const router = express.Router(); 
+
+const createUserSchema = Joi.object({
+    firstName: Joi.string().required(), 
+    lastName: Joi.string().required(),
+    email: Joi.string().required().email(),
+    rol: Joi.string().required().validate(['student', 'teacher']), 
+    date: Joi.date().default(Date.now),
+});
+
+
+router.post('/', body(createUserSchema), async (req, res) => {
     const body = req.body;     
     res.status(201).json({
         message: 'created',
@@ -12,3 +22,4 @@ router.post('/', (req, res) => {
 });
 
 export default router; 
+module.exports = {createUserSchema}; 
