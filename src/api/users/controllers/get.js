7c36c9d9fc0c33c74c0ee903ac27c86cd2b55690
jlorebu <jeibeltranu@gmail.com>
2023-05-16 22:45:06 -0500

    documentacion

--- src/api/users/controllers/get.js
+++ src/api/users/controllers/get.js
@@ -2,6 +2,26 @@ import User from '../../../models/users.js'
 
 
 //Listar usuarios
+/**
+ * @openapi
+ * /api/users:
+ *   get:
+ *     description: Users list
+ *     responses:
+ *       200:
+ *         description: arrayUsers
+ *         content: 
+ *           application/json:
+ *              schema:
+ *                type: array
+ *                items:
+ *                  type: object
+ *                  properties: 
+ *                    firstName: 
+ *                      type: String
+ *       400:
+ *         description: Bad request
+ */
 const getUsers = async (req, res, next) => {
 	try {
 		const arrayUsers = await User.find();
@@ -16,17 +36,46 @@ const getUsers = async (req, res, next) => {
 
 
 //buscar Id
-
-const getUserById = async (req, res) => {
-	const id = req.params.id;
-	const userId = await User.findById(id)
-
-	if (!userId) {
-		return res.status(404).json(
-			{
-				message: "user not found"
-			})
-	}
+/**
+ * @openapi
+ * /api/users/:id:
+ *   get:
+ *     description: Creation API for users
+ *     parameters:
+ *       - name: firstName
+ *         in: formData
+ *         type: string
+ *         required: true
+ *       - name: lastName
+ *         in: formData
+ *         type: string
+ *         required: true
+ *       - name: email
+ *         in: formData
+ *         type: string
+ *         required: true
+ *         match: /.+\@.+\..+/
+ *         unique: true
+ *       - name: rol
+ *         in: formData
+ *         type: string
+ *         required: true
+ *         enum: ['student', 'teacher']
+ *     responses:
+ *       200:
+ *         description: User created
+ *       400:
+ *         description: Bad request
+ */
+const getUserById =  async (req, res) => {
+    const id = req.params.id;
+    const userId = await User.findById(id)
+    
+    if(!userId){
+        return res.status(404).json(
+            {
+            message: "user not found"})
+        }
 
 	return res.status(200).json(
 		{
--- src/api/users/controllers/post.js
+++ src/api/users/controllers/post.js
@@ -1,5 +1,37 @@
 import User from '../../../models/users.js'
 
+/**
+ * @openapi
+ * /api/users:
+ *   post:
+ *     description: Creation API for users
+ *     parameters:
+ *       - name: firstName
+ *         in: formData
+ *         type: string
+ *         required: true
+ *       - name: lastName
+ *         in: formData
+ *         type: string
+ *         required: true
+ *       - name: email
+ *         in: formData
+ *         type: string
+ *         required: true
+ *         match: /.+\@.+\..+/
+ *         unique: true
+ *       - name: rol
+ *         in: formData
+ *         type: string
+ *         required: true
+ *         enum: ['student', 'teacher']
+ *     responses:
+ *       200:
+ *         description: User created
+ *       400:
+ *         description: Bad request
+ */
+
 const createUsers = async (req, res, next) => {
 	//peticion para crear el usuario - usar el modelo
 	try {
