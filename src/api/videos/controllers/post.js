ae507bbdba606d304f25fd3dafe3197646483903
jlorebu <jeibeltranu@gmail.com>
2023-05-24 15:20:44 -0500

    post doc

--- src/api/videos/controllers/post.js
+++ src/api/videos/controllers/post.js
@@ -1,5 +1,49 @@
 import Video from '../../../models/videos.js';
 
+/**
+ * @openapi
+ * /api/videos:
+ *   post:
+ *     summary: Create Video
+ *     tags: [Videos]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               url:
+ *                 type: string
+ *               tittle:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               liderFullName:
+ *                 type: string
+ *               liderEmail:
+ *                 type: string
+ *             required:
+ *               - url
+ *               - tittle
+ *               - description
+ *               - liderFullName
+ *               - liderEmail
+ *           example:
+ *             url: 'https://www.youtube.com/watch?v=2V-IYdcsoAw'
+ *             tittle: 'El Átomo'
+ *             description: 'Adjunto video del átomo.'
+ *             liderFullName: 'Pedro Pérez'
+ *             liderEmail: pedroperez@example.com
+ *     responses:
+ *       '201':
+ *         description: User Created
+ *       '400':
+ *         description: Bad Request
+ *       '500':
+ *         description: Error 
+ */
+
 const createVideos = async (req, res, next) => {
 	//peticion para crear el usuario - usar el modelo
 	try {
