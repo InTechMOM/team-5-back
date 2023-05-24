9d575fd7baae016454594e54e8e1d5a7fe20c8b6
jlorebu <jeibeltranu@gmail.com>
2023-05-24 15:29:45 -0500

    corrections doc

--- src/api/videos/controllers/get.js
+++ src/api/videos/controllers/get.js
@@ -4,40 +4,40 @@ import Video from "../../../models/videos.js";
 /**
  * @openapi
  * /api/videos:
- *  get:
- *   summary: Videos list
- *   tags: [Videos]
- *   responses:
- *     '200':
- *       description: List of videos
- *       content:
- *         application/json:
- *           schema:
- *             type: array
- *             items:
- *               type: object
- *               properties:
- *                 url:
- *                   type: string
- *                   example: 'https://www.youtube.com/watch?v=2V-IYdcsoAw'
- *                 tittle:
- *                   type: string
- *                   example: 'El átomo'
- *                 description:
- *                   type: string
- *                   example: 'Adjunto el video del átomo'
- *                 liderFullName:
- *                   type: string
- *                   example: 'Pedro Pérez'
- *                 liderEmail:
- *                   type: string
- *                   example: pedroperez@example.com
- *                 creationDate:
- *                   type: string
- *                   format: date-time
- *                   example: '2023-05-18T12:00:00Z'
- *     '404':
- *       description: Videos not found
+ *   get:
+ *     summary: Videos list
+ *     tags: [Videos]
+ *     responses:
+ *       '200':
+ *         description: List of videos
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   url:
+ *                     type: string
+ *                     example: 'https://www.youtube.com/watch?v=2V-IYdcsoAw'
+ *                   tittle:
+ *                     type: string
+ *                     example: 'El átomo'
+ *                   description:
+ *                     type: string
+ *                     example: 'Adjunto el video del átomo'
+ *                   liderFullName:
+ *                     type: string
+ *                     example: 'Pedro Pérez'
+ *                   liderEmail:
+ *                     type: string
+ *                     example: pedroperez@example.com
+ *                   creationDate:
+ *                     type: string
+ *                     format: date-time
+ *                     example: '2023-05-18T12:00:00Z'
+ *       '404':
+ *         description: Videos not found
  */
 
 
